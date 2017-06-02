d983cc842d02f9bac51ed6752b2395197e15ac44
KonH <konh@yandex.ru>
2017-06-03 00:35:33 +0700

    So dump prototype;

--- CrashAnalyzer/CrashAnalyzer.cs
+++ CrashAnalyzer/CrashAnalyzer.cs
@@ -7,6 +7,9 @@ namespace CrashAnalyzer {
 			var apkDir = UnzipApkFile(args.ApkPath);
 			var dump = new CrashDump(args.DumpPath);
 			dump.Resolve();
+
+			SoDumper dumper = new SoDumper("/Users/konh/Projects/Unity3D/UnityCrashAnalizer/Builds/build_mono/lib/armeabi-v7a/libmono.so");
+			dumper.Dump();
 		}
 
 		public static string UnzipApkFile(string path) {
---
+++ CrashAnalyzer/SoDumper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace CrashAnalyzer {
+	public class SoDumper {
+
+		string _path = null;
+
+		public SoDumper(string path) {
+			_path = path;
+		}
+
+		public void Dump() {
+			try {
+				Console.WriteLine($"Start dump file: '{_path}'");
+				var toolPath = "/Users/konh/Library/Developer/Xamarin/android-ndk/android-ndk-r10e/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-objdump";
+				var args = $"-S {_path} -C";
+				var startInfo = new ProcessStartInfo() {
+					FileName = toolPath,
+					Arguments = args,
+					UseShellExecute = false,
+					RedirectStandardOutput = true,
+					CreateNoWindow = true
+				};
+				var proc = Process.Start(startInfo);
+				proc.Start();
+				var output = proc.StandardOutput.ReadToEnd();
+				var outputPath = _path + ".txt";
+				File.WriteAllText(outputPath, output);
+				Console.WriteLine($"Dump file for '{_path}' is saved to '{outputPath}'");
+			} catch ( Exception e ) {
+				Console.WriteLine($"Failed to dump file: '{_path}': '{e.Message}'");
+			} 
+		}
+	}
+}
