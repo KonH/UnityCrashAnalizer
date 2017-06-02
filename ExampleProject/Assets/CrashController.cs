c6fa07b9919ceef6333f3d8dc5de08171f795ac5
KonH <konh@yandex.ru>
2017-06-02 20:54:22 +0700

    Example project with Instantiation long crash;

---
+++ ExampleProject/Assets/CrashController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+
+public class CrashController : MonoBehaviour {
+
+	public void PerformFileException() {
+		var lines = File.ReadAllLines(Guid.NewGuid().ToString());
+		Debug.Log(lines.Length);
+	}
+
+	public void StackOverflow() {
+		StackOverflow(0);
+	}
+
+	void StackOverflow(int x) {
+		StackOverflow(x++);
+	}
+
+	public void SetNull() {
+		GameObject go = null;
+		go.SetActive(false);
+	}
+
+	public void Instantiation() {
+		while ( true ) {
+			var go = new GameObject("");
+		}
+	}
+}
---
+++ ExampleProject/Assets/UILogger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UILogger : MonoBehaviour {
+
+	public Text Text;
+
+	void Awake () {
+		Application.logMessageReceived += Application_logMessageReceived;
+	}
+
+	public void Clear() {
+		Text.text = "";
+	}
+
+	void Application_logMessageReceived(string condition, string stackTrace, LogType type) {
+		var stackTraceShort = !string.IsNullOrEmpty(stackTrace) ? stackTrace.Split('\n')[0] : "";
+		Text.text += string.Format("\n[{0}] {1}: {2}\n", condition, type, stackTraceShort);
+	}
+}
